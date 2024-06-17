e41f6003ca99096f68eaa55a0e50ac1062838480
Pranjal0512 <pranjal.barnwal051234@gmail.com>
2024-06-17 14:40:03 +0545

    my second commit

--- vege/views.py
+++ vege/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from vege.models import *
 
 # Create your views here.
@@ -20,8 +20,31 @@ def recipesview(request):
     context={'recipes':queryset}
     return render(request, "recipeviews.html", context)
 
+def recipeupdate(request):
+    return render(request, "recipeupdate.html")
+
 def del_recipe(request,id):
     queryset = Recipe.objects.get(id=id).delete()
     context={'recipes':queryset}
     return render(request, "recipeviews.html", context)
+
+def update_recipe(request,id):
+    queryset = Recipe.objects.get(id = id)
+    if request.method == "POST":
+        recipe_image = request.FILES.get("recipe_image")   
+        recipe_name = request.POST.get("recipe_name")   
+        recipe_description = request.POST.get("recipe_description")  
+
+        
+        queryset.recipe_name=recipe_name,
+        queryset.recipe_description=recipe_description,
+        if recipe_image:
+            queryset.recipe_image=recipe_image
+        queryset.save()
+        
+        return redirect('recipesview')
+    
+    context = {'recipe':queryset}
+    
+    return render(request, "recipeupdate.html", context)
 
